181541ed558878b2c9193abb149fdc6f41e8da72
TechNext CRM <admin@technext.local>
2026-06-15 17:54:37 +0530

    style: increase logo size in updated branding areas

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -33,7 +33,7 @@ export default function Sidebar({ user }: { user: any }) {
       />
       <aside className={`${styles.sidebar} ${isMobileOpen ? styles.mobileOpen : ''}`}>
       <div className={styles.logoContainer}>
-        <img src="https://res.cloudinary.com/dwzerbhuj/image/upload/q_auto/f_auto/v1776917252/Untitled-2_gx7mta.png" alt="TECHNEXT Logo" style={{ width: '150px', height: 'auto', filter: 'brightness(0)' }} className={styles.logo} />
+        <img src="https://res.cloudinary.com/dwzerbhuj/image/upload/q_auto/f_auto/v1776917252/Untitled-2_gx7mta.png" alt="TECHNEXT Logo" style={{ width: '220px', height: 'auto', filter: 'brightness(0)' }} className={styles.logo} />
       </div>
       
       <nav className={styles.nav}>
